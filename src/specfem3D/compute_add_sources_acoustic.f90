04edf424bfe66d6a7693d711860f6e67ecc0c2db
Youshan Liu <ysliu@mail.iggcas.ac.cn>
2025-02-26 20:04:33 +0800

    Update compute_add_sources_acoustic.f90

--- src/specfem3D/compute_add_sources_acoustic.f90
+++ src/specfem3D/compute_add_sources_acoustic.f90
@@ -94,11 +94,25 @@
           time_source_dble = time_t - tshift_src(isource)
 
           ! determines source time function value
-          stf = get_stf_acoustic(time_source_dble,isource)
+          if ((1 == SIMULATION_TYPE) .and. (1 == time_stepping_scheme)) then
+            ! For forward simulation using Newmark format, the source wavelet function at t+deltat time instant should be used in correction phase because a^(n+1) should be used.
+             stf = get_stf_acoustic(time_source_dble+DT,isource)
+          else
+             stf = get_stf_acoustic(time_source_dble,isource)
+          end if
 
           !! VM VM add external source time function
           if (USE_EXTERNAL_SOURCE_FILE) then
-            stf = user_source_time_function(it, isource)
+             if ((1 == SIMULATION_TYPE) .and. (1 == time_stepping_scheme)) then
+                ! For forward simulation using Newmark format, the source wavelet function at t+deltat time instant should be used in correction phase because a^(n+1) should be used.
+                if (it < NSTEP) then
+                   stf = user_source_time_function(it+1, isource)
+                else
+                   stf = 0.d0
+                end if
+             else
+               stf = user_source_time_function(it, isource)
+             end if
           endif
 
           ! distinguishes between single and double precision for reals
